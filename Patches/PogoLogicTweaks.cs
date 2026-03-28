a55a39012bd958771d58fa80a1fbaba0a6363ae1
kaycodes13 <egad.coding@gmail.com>
2026-03-27 21:01:19 -0400

    fix a vanilla bug that triggered pogo bounces twice

---
+++ Patches/PogoLogicTweaks.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace Needleforge.Patches;
+
+[HarmonyPatch(typeof(HeroDownAttack))]
+internal static class PogoLogicTweaks
+{
+    // Fixes a vanilla bug where NailSlash down attacks trigger two auto-bounces
+    [HarmonyPatch("OnEndedDamage")]
+    [HarmonyPrefix]
+    static void PreventDoubleBounces(HeroDownAttack __instance, bool didHit)
+    {
+        if (didHit && __instance.bounceQueued && __instance.attack)
+            __instance.hc.AffectedByGravity(true);
+        __instance.bounceQueued = false;
+    }
+}
